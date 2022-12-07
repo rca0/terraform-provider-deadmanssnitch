4c6e7e630c0ac27fd8ce86fc0d89f617bca2c12f
Ruan Arcega <ruanarcega@gmail.com>
2022-12-07 02:09:04 -0300

    feat: add new terraform example

---
+++ examples/main.tf
@@ -0,0 +1,33 @@
+provider "deadmanssnitch" {
+  // ENV VARS: DEADMANSSNITCH_APIKEY DEADMANSSNITCH_BASEURL 
+  // baseurl = "https://api.deadmanssnitch.com/v1/snitches" (default value)
+  apikey = var.deadmanssnitch_api_key
+}
+
+resource "deadmanssnitch_snitch" "foo" {
+  name       = "foo"
+  interval   = "2_minute"
+  alert_type = "basic"
+  alert_mail = "me@ruanc.dev"
+  notes      = "The sample example of snitch creation with terraform"
+  tags = [
+    "basic",
+    "production",
+    "critical",
+    "terraform"
+  ]
+}
+
+resource "deadmanssnitch_snitch" "bar" {
+  name       = "bar"
+  interval   = "weekly"
+  alert_type = "smart"
+  alert_mail = "me@ruanc.dev"
+  notes      = "The sample example of snitch creation with terraform"
+  tags = [
+    "smart",
+    "production",
+    "critical",
+    "terraform"
+  ]
+}
